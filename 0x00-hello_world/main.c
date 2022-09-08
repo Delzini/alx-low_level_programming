6202ebede456394ea07e7575ab4ec9addc1cc58c
Ayodeji <delzini247@gmail.com>
2022-09-08 16:05:57 -0700

    task 0

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-/**
-* main - Entry point
-*
-* Description: description for function main
-* 
-* Return: Always 0 (Success)
-*/
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("Programming is understanding grammatical syntax")
 return (0);
 }
