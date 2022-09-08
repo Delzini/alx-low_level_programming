58add40aef325db74003ad7357a02e4641d5d41f
Ayodeji <delzini247@gmail.com>
2022-09-08 15:39:40 -0700

    task o preprocessor

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: description for function main
+* 
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+puts("Programming is understanding grammatical syntax")
+return (0);
+}
