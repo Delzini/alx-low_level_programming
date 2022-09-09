986da30232fbd16db5a93eb7d5158298575245ab
Ayodeji <delzini247@gmail.com>
2022-09-09 13:43:31 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+
+ * main - Prints the alphabetic
+
+ *
+
+ * Return: Always (Success)
+
+ */
+
+int main(void)
+
+{
+
+char x;
+
+
+
+for (x = 'a'; x <= 'y'; x++)
+
+putchar(x);
+
+putchar('\n');
+
+return (0);
+
