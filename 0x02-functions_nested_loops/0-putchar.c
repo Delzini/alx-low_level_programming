fe643ee26a9dfd8cbad455b3b2b2b649fa8d2502
Ayodeji <delzini247@gmail.com>
2022-09-13 09:52:03 -0700

    task 0 printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+
+	return (0);
+}
