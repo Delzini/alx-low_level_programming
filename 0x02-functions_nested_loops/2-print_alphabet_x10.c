31dbe3b59cce72b006dfd641b304ca8a2e8fdf3a
Ayodeji <delzini247@gmail.com>
2022-09-13 13:27:44 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a to z x10
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0; i <= 9; i++)
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(i);
+	}
+_putchar('\n');
+}
+};
