796287b60c272b4e1b95df4a0772383c8facc1d1
Ayodeji <delzini247@gmail.com>
2022-09-19 16:47:04 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other in string.
+ * @str: pointer to string
+ * Return: void.
+ */
+
+void puts2(char *str)
+{
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		if (n % 2 == 0)
+		{
+			putchar(str[n]);
+		}
+		n++;
+	}
+	putchar('\n');
+}
