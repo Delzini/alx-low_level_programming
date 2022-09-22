5963c5496806e97f62e665299d2147ab75f9f3db
Ayodeji <delzini247@gmail.com>
2022-09-22 16:16:41 -0700

    task 5,6,7

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,21 +11,12 @@ char *string_toupper(char *p)
 	int a = 0;
 
 	while (p[a])
-
-					{
-
-								if (p[a] >= 97 && p[a] <= 122)
-
-											{
-
-															p[a] -= 32;
-
-																	}
-
-										a++;
-
-											}
-
-				return (p);
-
+	{
+		if (p[a] >= 97 && p[a] <= 122)
+		{
+			p[a] -= 32;
+		}
+		a++;
+	}
+	return (p);
 }
