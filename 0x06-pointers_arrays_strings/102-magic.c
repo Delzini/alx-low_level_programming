d85287a9eac716d7001395b17917924323244f32
Ayodeji <delzini247@gmail.com>
2022-09-22 17:24:36 -0700

    task

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - write your line of code here..
- * Remember:
  * you are not allowed to use a.
  * you are not allowed to modify p.
  * Return: void
