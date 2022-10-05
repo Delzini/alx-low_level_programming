636c9e11c53cb3735b8f7546e9cc108b2ed610ad
Ayodeji <delzini247@gmail.com>
2022-10-05 06:36:38 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,6 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	arr = malloc(sizeof(char) * size);
-	/* check if malloc was successful */
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
